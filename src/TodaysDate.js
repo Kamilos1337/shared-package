a7412488df75085becf2e753bc33a62e8470e698
k.zachradnik <k.zachradnik@gmail.com>
2023-01-16 12:16:30 +0100

    updated

--- src/TodaysDate.js
+++ src/TodaysDate.js
@@ -2,12 +2,11 @@ import React, {useState} from "react";
 
 function TodaysDate() {
     // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
 
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <p>You clicked times</p>
+            <button>
                 Click me
             </button>
         </div>
